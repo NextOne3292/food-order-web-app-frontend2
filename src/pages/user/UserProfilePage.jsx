cfe07cf249e696be6927f4d22eb4c737bb963aa3
Aswathyanu ECE <aswathyanuece@gmail.com>
2025-04-21 11:31:27 +0530

    Updated UserProfilePage.jsx

--- src/pages/user/UserProfilePage.jsx
+++ src/pages/user/UserProfilePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { axiosInstance } from "../../config/axiosInstance";
 import { useSelector, useDispatch } from "react-redux";
-import { updateUser } from "../../redux/userSlice";
+import { updateUser } from "../../redux/UserSlice";
 import UserSidebar from "../../components/User/UserSidebar";
 import UserEditForm from "../../components/User/UserEditForm";
 
