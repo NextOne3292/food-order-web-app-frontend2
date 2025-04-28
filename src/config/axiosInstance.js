10bcf5bb1d36169cc2b7aa493e1561cfe8071aba
Aswathyanu ECE <aswathyanuece@gmail.com>
2025-04-28 17:40:03 +0530

    Updated axiosInstance.js to dynamically set base URL

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+// Dynamically detect base URL (for production) or fall back to the VITE_BASE_URL from the .env file
+const baseUrl = window.location.origin || import.meta.env.VITE_BASE_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
+    baseURL: `${baseUrl}/api`, // This will use window.location.origin in production and VITE_BASE_URL locally
     withCredentials: true,
 });
